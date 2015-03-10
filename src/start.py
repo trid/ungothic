7a932173f5178d72febabd108f27342c6672db62
TriD <hovyakov@gmail.com>
2015-03-10 02:15:28 +0200

    sdl2uipy test library

--- src/start.py
+++ src/start.py
@@ -1,5 +1,14 @@
 __author__ = 'TriD'
 
 import sdl2uipy
-print sdl2uipy.greet()
 
+sdl2uipy.video_init()
+
+image = sdl2uipy.Image.from_file('../res/images/castle.jpg')
+
+while True:
+    image.draw(0, 0)
+    sdl2uipy.flip_screen()
+    event = sdl2uipy.get_event()
+    if event.type == sdl2uipy.EventType.EVENT_TYPE_QUIT:
+        break
