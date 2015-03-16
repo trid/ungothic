5bcf1dcbff31db94a92a16427abe8358f0f5d119
TriD <hovyakov@gmail.com>
2015-03-16 09:50:35 +0200

    Starting work on game state

---
+++ ungothic/state.py
@@ -0,0 +1,15 @@
+__author__ = 'TriD'
+
+
+class State(object):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def on_push(self):
+        pass
+
+    def on_pop(self):
+        pass
