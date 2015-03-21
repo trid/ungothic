4d4f8ed6e0aad6203550d36accfa8f0c640dd13c
TriD <hovyakov@gmail.com>
2015-03-22 01:27:42 +0200

    Fixed video to use sprite manager as singleton

--- ungothic/view/view.py
+++ ungothic/view/view.py
@@ -1,5 +1,5 @@
 import sdl2uipy
-from ungothic.view import SpriteManager
+from ungothic.view import get_sprite_manager
 from ungothic.view.ui_manager import UIManager
 
 __author__ = 'TriD'
@@ -9,10 +9,9 @@ class View(object):
     def __init__(self, application):
         sdl2uipy.video_init()
         self.application = application
-        self.sprite_manager = SpriteManager()
         self.ui_manager = UIManager(self)
 
     def draw(self):
-        self.sprite_manager['castle_bg'].draw(0, 0)
+        get_sprite_manager()['castle_bg'].draw(0, 0)
         self.ui_manager.draw()
         sdl2uipy.flip_screen()
