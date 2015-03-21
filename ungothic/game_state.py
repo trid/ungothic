3808547edd3dcb61e189b3f38d6775bab9b6bcad
TriD <hovyakov@gmail.com>
2015-03-22 01:39:46 +0200

    Started game character menu

--- ungothic/game_state.py
+++ ungothic/game_state.py
@@ -1,5 +1,7 @@
 from ungothic.application import application
 from ungothic.state import State
+from ungothic.view import get_sprite_manager
+from ungothic.view.specific_controls.characters_menu import CharactersMenu
 from ungothic.view.ui.button import Button
 
 __author__ = 'TriD'
@@ -8,19 +10,22 @@ __author__ = 'TriD'
 class GameState(State):
     def __init__(self):
         super(GameState, self).__init__()
-        characters_button_image = application.view.sprite_manager['button_characters']
+        characters_button_image = get_sprite_manager()['button_characters']
         self.characters_button = Button(0, 0, characters_button_image, self.show_characters)
+        self.characters_menu = CharactersMenu(0, 30, 800, 570)
 
     def on_push(self):
         super(GameState, self).on_push()
         application.view.ui_manager.add_item(self.characters_button)
+        application.view.ui_manager.add_item(self.characters_menu)
 
     def on_pop(self):
         super(GameState, self).on_pop()
         application.view.ui_manager.remove_item(self.characters_button)
+        application.view.ui_manager.remove_item(self.characters_menu)
 
     def update(self):
         super(GameState, self).update()
 
     def show_characters(self):
-        pass
+        self.characters_menu.visible = True
--- ungothic/view/specific_controls/characters_menu.py
+++ ungothic/view/specific_controls/characters_menu.py
@@ -1,7 +1,13 @@
+from ungothic.view.sprites import get_sprite_manager
+from ungothic.view.ui.button import Button
 from ungothic.view.ui.panel import Panel
 
 __author__ = 'TriD'
 
 
 class CharactersMenu(Panel):
-    pass
+    def __init__(self, x, y, w, h):
+        super(CharactersMenu, self).__init__(x, y, w, h)
+        self.button_create = Button(0, 0, get_sprite_manager()['button_create'], None)
+        self.add_item(self.button_create)
+        self.visible = False
